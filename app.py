76e3183bb241acfb8a72fe51884f5090e2573187
arash javaheripour <arashjp@Arashs-MacBook-Pro.local>
2025-02-11 12:19:53 +0330

    latest

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from utility import UtilityClass
 from kafka_producer_layer import KafkaProducerLayer
 import schedule
+import json
 
 
 def send_message():
-    result= UtilityClass.send_get_request("http://randomuser.me/api/", {"results": 100})
+    result= UtilityClass.send_get_request("http://randomuser.me/api/", {"results": 100, "nat": "ir"})
     if result  == None:
         return
-    kfka = KafkaProducerLayer("localhost", 9092, "users")
-    kfka.produce(result)
+    kfka = KafkaProducerLayer("localhost", 19092, "users")
+    for user in result:
+        kfka.produce(json.dumps(user))
+
     
         
 def run_app(): 
--- kafka_producer_layer.py
+++ kafka_producer_layer.py
@@ -1,12 +1,21 @@
 from kafka import KafkaProducer
-
+from kafka.errors import KafkaError
+from log_utility import LoggerLayer
 class KafkaProducerLayer():
     def __init__(self, host, port, topic):
-        self.producer = KafkaProducer(bootstrap_servers="{}:{}".format(host, port))
+        self.producer = KafkaProducer(bootstrap_servers="{}:{}".format(host, port),
+                                      value_serializer = lambda x: x.encode('utf-8'))
         self.topic = topic
+        self.logger = LoggerLayer()
+        
 
     def produce(self, message):
-        self.producer.send(self.topic, message)
-        self.producer.flush()
+        try:
+            self.producer.send(self.topic, message)
+            self.producer.flush()
+            self.logger.log_info(f"message sent to topic {self.topic}")
+        except KafkaError as e:
+            self.logger.log_error(f"error while sending message to topic {self.topic} : {e}")
+
 
         
